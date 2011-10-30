3a02ef959ffc55ab143a13049077b0122867da67
Martin Trejo Chavez <chilli.coder@gmail.com>
2011-10-30 12:06:36 -0600

    triangle project just done

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -15,6 +15,18 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  sides = Array.new
+  sides << a << b << c
+  sides.uniq!
+
+  case sides.size
+  when 1
+    return :equilateral
+  when 2
+    return :isosceles
+  when 3
+    return :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
